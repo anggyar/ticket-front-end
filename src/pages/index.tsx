6284131a89f4384a04b8ce84c1e700801836d76c
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-10-20 22:52:20 +0700

    feat: implement custom react component for Heading Page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import { Button } from "@heroui/react";
+import PageHead from "@/components/commons/PageHead";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <Button color='primary'>Test Button</Button>
-    </div>
+      <PageHead title='Acara' />
+      <Button color='primary'>Button</Button>
+    </main>
   );
 }
