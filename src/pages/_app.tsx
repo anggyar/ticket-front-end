d5a16d07288b85812093e256f87783be76668e40
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-10-23 01:01:18 +0700

    feat: implement query client library from tanstack/react-query. Now user can fetching and synchronize data more easily

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,24 +3,36 @@
 import "@/styles/styles.css";
 import { cn } from "@/utils/cn";
 import { HeroUIProvider } from "@heroui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Inter } from "next/font/google";
 const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <HeroUIProvider>
-      <main
-        className={cn(
-          inter.className,
-          "flex min-h-screen min-w-full flex-col items-center justify-center gap-4"
-        )}
-      >
-        <Component {...pageProps} />
-      </main>
-    </HeroUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <HeroUIProvider>
+        <main
+          className={cn(
+            inter.className,
+            "flex min-h-screen min-w-full flex-col items-center justify-center gap-4"
+          )}
+        >
+          <Component {...pageProps} />
+        </main>
+      </HeroUIProvider>
+    </QueryClientProvider>
   );
 }
 
