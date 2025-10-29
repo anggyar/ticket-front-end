c833dcd1101f6ce40071ff81fe5e6f9cf9350d85
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-10-30 03:00:24 +0700

    feat: add AUTH_SECRET to environment config and remove unused API route

--- src/pages/auth/activation/index.tsx
+++ src/pages/auth/activation/index.tsx
@@ -18,7 +18,6 @@ export async function getServerSideProps(context: { query: { code: string } }) {
   try {
     const result = await authServices.activation({ code: context.query.code });
 
-    console.log("activation result:", result);
     if (result.data.data) {
       return {
         props: {
